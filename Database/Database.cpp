30afdafa2ce0a84b77ee149ba6b10fc1cf8127af
YuqqiZhou <41812724+YuqqiZhou@users.noreply.github.com>
2019-05-14 17:28:19 +0800

    Add files via upload

--- Database/Database.cpp
+++ Database/Database.cpp
@@ -123,6 +123,8 @@ Database::Database(string _name)
 
 	this->id_correspondence_file = "id_correspondence.txt";
 	this->exist_id_correspondence_file = this->load_id_correspondence_file();
+	this->kvstore->fillmap_pre2so(this->preid2subobj);
+	this->kvstore->fillmap_so2pre(this->subobjid2pre);
 }
 
 //==================================================================================================================================================
